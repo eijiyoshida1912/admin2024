95b63ab3a30fa2c807c2b2c21cf67ff33667adb8
Yoshida.Eiji <yoshida.eiji@trans-cosmos.co.jp>
2024-08-02 18:08:29 +0900

    widgetを入れ替え可能にした

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import Chart from "../../components/chart/Chart";
 import Featured from "../../components/featured/Featured";
 import Navbar from "../../components/navbar/Navbar";
@@ -8,20 +8,68 @@ import Widget from "../../components/widget/Widget";
 import "./home.scss";
 import { LanguageContext } from "../../context/languageContext";
 import text from "../../text.json";
+import {
+  DndContext,
+  closestCenter,
+  KeyboardSensor,
+  PointerSensor,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core";
+import {
+  arrayMove,
+  horizontalListSortingStrategy,
+  SortableContext,
+  sortableKeyboardCoordinates,
+  verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
+import { SortableItem } from "./SortableItem";
 
 const Home = () => {
   const { language } = useContext(LanguageContext);
+
+  const [items, setItems] = useState(["user", "order", "earning", "balance"]);
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    })
+  );
+
+  function handleDragEnd(event) {
+    const { active, over } = event;
+
+    if (active.id !== over.id) {
+      setItems((items) => {
+        const oldIndex = items.indexOf(active.id);
+        const newIndex = items.indexOf(over.id);
+
+        return arrayMove(items, oldIndex, newIndex);
+      });
+    }
+  }
 
   return (
     <div className="home">
       <Sidebar />
       <div className="homeContainer">
         <Navbar />
+
         <div className="widgets">
-          <Widget type="user" />
-          <Widget type="order" />
-          <Widget type="earning" />
-          <Widget type="balance" />
+          <DndContext
+            sensors={sensors}
+            collisionDetection={closestCenter}
+            onDragEnd={handleDragEnd}
+          >
+            <SortableContext
+              items={items}
+              strategy={horizontalListSortingStrategy}
+            >
+              {items.map((id) => (
+                <SortableItem key={id} id={id} />
+              ))}
+            </SortableContext>
+          </DndContext>
         </div>
         <div className="charts">
           <Featured />
